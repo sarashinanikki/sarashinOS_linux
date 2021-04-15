f7291f1024b867b004e137b0607dd2e67db0d609
Natsuki YOKOTA <natsuki.yokota2@persol.co.jp>
2021-04-15 17:42:24 +0900

    Update: finish chapter 8

--- src/bootpack.c
+++ src/bootpack.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+
 void io_hlt(void);
 void io_cli(void);
 void io_out8(int port, int data);
@@ -8,9 +9,12 @@ void io_store_eflags(int eflags);
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-void init_screen(char *vram, int x, int y);
+void init_screen8(char *vram, int x, int y);
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
-void putfont8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *str);
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *str);
+void init_mouse_cursor8(char *mouse, char bc);
+void putblock8_8(char *vram, int vxsize, int pxsize,
+	int pysize, int px0, int py0, char *buf, int bxsize);
 
 #define COL8_000000		0
 #define COL8_FF0000		1
@@ -38,15 +42,19 @@ struct BOOTINFO {
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
-	char s[40];
+	char s[40], mcursor[256];
+	int mx, my;
 
 	init_palette();
-	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
-
-	putfont8_asc(binfo->vram, binfo->scrnx, 21, 21, COL8_000000, "sarashinOS");
-	putfont8_asc(binfo->vram, binfo->scrnx, 20, 20, COL8_FFFFFF, "sarashinOS");
-	sprintf(s, "scrnx = %d", binfo->scrnx);
-	putfont8_asc(binfo->vram, binfo->scrnx, 120, 80, COL8_FFFFFF, s);
+	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+	mx = (binfo->scrnx - 16) / 2; /* 画面中央になるように座標計算 */
+	my = (binfo->scrny - 28 - 16) / 2;
+	// マウスカーソルのデータを作る
+	init_mouse_cursor8(mcursor, COL8_008484);
+	// マウスカーソルのデータを書き込む
+	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+	sprintf(s, "(%d, %d)", mx, my);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 8, 8, COL8_FFFFFF, s);
 
 	for (;;) {
 		io_hlt();
@@ -106,7 +114,7 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 	return;
 }
 
-void init_screen(char *vram, int x, int y)
+void init_screen8(char *vram, int x, int y)
 {
 	boxfill8(vram, x, COL8_008484,  0,     0,      x -  1, y - 29);
 	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 28, x -  1, y - 28);
@@ -141,11 +149,65 @@ void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
 	return;
 }
 
-void putfont8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *str)
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *str)
 {
 	extern char hankaku[4096];
 	for (; *str != 0x00; str++) {
 		putfont8(vram, xsize, x, y, c, hankaku + (*str) * 16);
 		x += 8;
 	}
+}
+
+void init_mouse_cursor8(char *mouse, char bc)
+/* マウスカーソルを準備（16x16） */
+{
+	static char cursor[16][16] = {
+		"**************..",
+		"*OOOOOOOOOOO*...",
+		"*OOOOOOOOOO*....",
+		"*OOOOOOOOO*.....",
+		"*OOOOOOOO*......",
+		"*OOOOOOO*.......",
+		"*OOOOOOO*.......",
+		"*OOOOOOOO*......",
+		"*OOOO**OOO*.....",
+		"*OOO*..*OOO*....",
+		"*OO*....*OOO*...",
+		"*O*......*OOO*..",
+		"**........*OOO*.",
+		"*..........*OOO*",
+		"............*OO*",
+		".............***"
+	};
+	int x, y;
+
+	for (y = 0; y < 16; y++) {
+		for (x = 0; x < 16; x++) {
+			if (cursor[y][x] == '*') {
+				mouse[y * 16 + x] = COL8_000000;
+			}
+			if (cursor[y][x] == 'O') {
+				mouse[y * 16 + x] = COL8_FFFFFF;
+			}
+			if (cursor[y][x] == '.') {
+				mouse[y * 16 + x] = bc;
+			}
+		}
+	}
+	return;
+}
+
+// pxsize = 横のピクセル数, pysize = 縦のピクセル数
+// (px, py) = 左上の座標
+// bxsize = 1ラインが何ピクセルで出来ているか
+void putblock8_8(char *vram, int vxsize, int pxsize,
+	int pysize, int px0, int py0, char *buf, int bxsize)
+{
+	int x, y;
+	for (y = 0; y < pysize; y++) {
+		for (x = 0; x < pxsize; x++) {
+			vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+		}
+	}
+	return;
 }
