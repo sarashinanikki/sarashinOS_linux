0e272f2a3f547a1768f16e52b4e6f7144dde6c3b
Natsuki YOKOTA <natsuki.yokota2@persol.co.jp>
2021-04-22 09:35:02 +0900

    Update: finish chapter 2

--- src/int.c
+++ src/int.c
@@ -27,18 +27,18 @@ void init_pic(void)
 
 #define PORT_KEYDAT		0x0060
 
+struct KEYBUF keybuf;
+
 void inthandler21(int *esp)
 /* PS/2キーボードからの割り込み */
 {
-	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-	unsigned char data, s[4];
+	unsigned char data;
 	io_out8(PIC0_OCW2, 0x61);	/* IRQ-01受付完了をPICに通知 */
 	data = io_in8(PORT_KEYDAT);
-
-	sprintf(s, "%02X", data);
-	boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
-	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
-
+	if (keybuf.flag == 0) {
+		keybuf.data = data;
+		keybuf.flag = 1;
+	}
 	return;
 }
 
